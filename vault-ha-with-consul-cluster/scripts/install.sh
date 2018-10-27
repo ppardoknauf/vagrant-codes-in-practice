3100fd7215975df6c2c3a8788e6d35f0d9da6604
Jamal Shahverdiev <jamal.shahverdiev@gmail.com>
2018-10-27 19:03:23 +0300

    Update install.sh

--- vault-ha-with-consul-cluster/scripts/install.sh
+++ vault-ha-with-consul-cluster/scripts/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 sudo yum clean all && sudo yum update -y
 sudo yum -y install epel-release && sudo yum -y install bash-completion net-tools bind-utils wget telnet vim expect gcc-c++ make jq unzip nfs-utils sshpass
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.rpm"
-sudo yum localinstall -y /home/vagrant/jdk-8u181-linux-x64.rpm
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.rpm"
+sudo yum localinstall -y /home/vagrant/jdk-8u191-linux-x64.rpm
 sudo sed -i.bak -e 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo systemctl restart sshd
 sudo systemctl stop firewalld && sudo systemctl disable firewalld
